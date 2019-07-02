8f825a24b4af832d10ac943d879bbd0b2f9baeb7
mingmingc <caressi.m@gmail.com>
2019-07-01 17:45:03 -0700

    removed userInput file, considated all to App.js:

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,127 @@
-import React from 'react';
-import UserInput from "./components/UserInput";
+import React, { Component } from 'react';
+import { Button } from "react-bulma-components/full";
 import './App.css';
 
-function App() {
-  return <UserInput />;
+// Next steps:
+// 1) Refactor the form 
+// 2) Dropdown/selection for operation ("select", etc)
+// bonus: describe each operation
+// 3) separate library for fetchResults function ("newton.js" to import later)
+// Order of ops
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //to track when results from API have loaded
+      isLoaded: false,
+      operation: "",
+      expression: "",
+      result: "",
+      error: {}
+    }
+  };
+
+  //Separate into another library to import later
+  //Function takes operation + expression (given by user):
+  // 1) URL encode expression
+  // 2) build URL
+  fetchResults(op, expr) {
+    this.setState({
+      //loading
+      isLoaded: false, //display breadcrumbs, spinner, etc
+      result: "",
+      error: {}
+    });
+
+    const encodedExpr = encodeURI(expr); //take user input & encode expression into URL format
+    const url = `https://newton.now.sh/${op}/${encodedExpr}`
+    fetch(url) //consider using a dropdown for the operations
+      .then(res => res.json())    //turn results to JSON obj
+      .then(({ operation, expression, result }) => {     //We use arrow function to unbind 'this', so 'this' refers to the instance of UserInput object as defined above 
+        console.log({ operation, expression, result }); //object descontruction
+        this.setState({
+          isLoaded: true,
+          operation, expression, result
+        })
+      })
+      .catch((error) => this.setState({ error }))
+  }
+
+  updateExpression(event) {
+    this.setState({
+      expression: event.target.value
+    })
+  }
+
+  render() {
+    const { operation, expression, result, error } = this.state;
+    console.log(operation);
+    console.log(expression);
+    console.log(result);
+    console.log(error);
+    // const op = 'derive';
+    // const expr = 'x^2';
+    return (
+      <div className="container">
+        <h1 className="title is-1"> Ming Newton </h1>
+        Calculator for all of your advanced math needs.
+        <div className="level">
+          {/* Level left: dropdown & form for user to select operator & enter expression */}
+          <div className="level-left">
+            <div className="level-item">
+              <p className="control is-expanded">
+                {/* Select operator */}  
+                <div class="select">
+                  <select>
+                    <option>Choose operator</option>
+                    <option>Simplify</option>
+                    <option>Factor</option>
+                    <option>Derive</option>
+                    <option>Integrate</option>
+                    <option>Find 0's</option>
+                    <option>Find Tangent</option>
+                    <option>Area Under Curve</option>
+                    <option>Cosine</option>
+                    <option>Sine</option>
+                    <option>Tangent</option>
+                    <option>Inverse Cosine</option>
+                    <option>Inverse Sine</option>
+                    <option>Inverse Tangent</option>
+                    <option>Absolute Value</option>
+                    <option>Logarithm</option>
+                  </select>
+                </div>
+                {/* Enter */}
+                <input className="input is-info is-fullwidth" type="text" placeholder="Enter what you want to calculate" 
+                onChange={() => this.updateExpression()} />
+              </p>
+            </div>
+          </div>
+          {/* Button */}
+          <div className="level-right">
+            <div className="level-item">
+              <p className="control">
+                {/* Onclick points to fetchResults function */}
+                <Button color="success" size="large">Wowza!</Button>
+                {/* rounded outlined onClick={() => this.fetchResults(op, expr)} */}
+              </p>
+            </div>
+            {/* results div  */}
+          </div>
+        </div>
+        <div>
+            {/* make display results conditional as long as no error */}
+            {operation} {' '}
+            {expression} =
+            {result}
+            {/* give user more specific info about error */}
+            {/* {error ? error : ""}  */}
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;

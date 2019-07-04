053c1bfdd41a5b700808319083b88a7102ad3c7f
mingmingc <caressi.m@gmail.com>
2019-07-03 17:20:00 -0700

    removed client directory

---
+++ build/precache-manifest.7d15395b51a346b63fd8d5af824d6f1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4871becef8d791120ca8",
+    "url": "/static/js/main.1d90a259.chunk.js"
+  },
+  {
+    "revision": "11f4cb3cf34c3ab4f272",
+    "url": "/static/js/2.a8423354.chunk.js"
+  },
+  {
+    "revision": "4871becef8d791120ca8",
+    "url": "/static/css/main.0bd731b9.chunk.css"
+  },
+  {
+    "revision": "5eade0aad7b64a8aab26ec62db22f22e",
+    "url": "/index.html"
+  }
+];
